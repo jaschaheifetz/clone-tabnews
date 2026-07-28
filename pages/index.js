bb73d5ba90d80c9b3c475fde9789c15c99bd43e9
Antonio Bernardo <antoniosantos_ccb@hotmail.com>
2026-07-28 13:36:50 -0300

    Adicionando outros arquivos

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Tabnews Clone!</h1>
+      <p>
+        Este é um projeto de implementação do Tabnews para treinar programação.
+      </p>
+      <p>
+        Esse projeto foi criado como parte de um curso de desenvolvimento web e tem como objetivo fornecer uma experiência semelhante ao Tabnews, permitindo que os usuários compartilhem notícias e interajam com a comunidade.
+      </p>
+      <p>
+        Sinta-se à vontade para explorar o projeto, enviar feedback e contribuir para o seu desenvolvimento. Esperamos que você aproveite a experiência!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
