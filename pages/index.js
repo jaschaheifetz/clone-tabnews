86527b2e620ab218b368872303c3fbd939f20dea
Antonio Bernardo <antoniosantos_ccb@hotmail.com>
2026-07-30 21:19:53 -0300

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 function Home() {
   return (
-    <div>
-      <h1>Bem-vindo ao Tabnews Clone! 😎</h1>
-      <p>
-        Este é um projeto de implementação do Tabnews para treinar programação.
-      </p>
-      <p>
-        Esse projeto foi criado como parte de um curso de desenvolvimento web e tem como objetivo fornecer uma experiência semelhante ao Tabnews, permitindo que os usuários compartilhem notícias e interajam com a comunidade.
-      </p>
-      <p>
-        Sinta-se à vontade para explorar o projeto, enviar feedback e contribuir para o seu desenvolvimento. Esperamos que você aproveite a experiência!
-      </p>
-    </div>
+      <div>
+          <h1>Bem-vindo ao Tabnews Clone! 😎</h1>
+
+          <form action="#" style={{ display: 'flex', flexDirection: 'column', gap: '20px', margin: '40px auto', maxWidth: '400px', padding: '40px 72px', border: '1px solid #ccc', borderRadius: '8px' }}>
+
+            <label htmlFor="email" style={{ fontWeight: 'bold', color: '#f93232', fontSize: '20px' }}>Insira seu e-mail para receber atualizações:</label>
+
+            <input style={{ padding: '10px', fontSize: '16px', borderRadius: '4px', border: '1px solid #ccc' }} type="email" id="email" name="email" placeholder="Digite seu e-mail" required />
+
+            <button style={{ padding: '10px', fontSize: '16px', borderRadius: '4px', border: '1px solid #ccc', backgroundColor: '#f93232', color: '#fff', cursor: 'pointer' }} type="submit">Enviar</button>
+            
+          </form>
+      </div>
   );
 }
 
